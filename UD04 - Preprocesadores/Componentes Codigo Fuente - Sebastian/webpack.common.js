9e4d8bc3aa8f24d518ef23a2b0dcec49e488b15d
sebasbermuher <alum.sbermudezh@iesalixar.org>
2022-02-11 21:17:10 +0100

    Entrega Tarea2-Componentes

---
+++ UD04 - Preprocesadores/Componentes Codigo Fuente - Sebastian/webpack.common.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  entry: {
+    scripts: "./assets/src/js/scripts.js",
+  },
+  plugins: [new CleanWebpackPlugin()],
+  output: {
+    path: path.resolve(__dirname, "assets/dist/js/"),
+    filename: "[name].js",
+  },
+};
